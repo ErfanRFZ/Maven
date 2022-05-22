41b48b7206951bf4624b1d57e6c0ba091c49ee4f
Erfan Rafezi <erfan.rafezi80@gmail.com>
2022-05-22 17:16:07 +0430

    Using Fibonacci lib in Maven app.

--- Maven_App/src/main/java/Main.java
+++ Maven_App/src/main/java/Main.java
@@ -1,6 +1,15 @@
+import lib.Fibonacci;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
+        System.out.println("FibonacciSequence:");
+        System.out.println("Enter the index of Fibonacci Sequence");
 
+        Scanner userInput = new Scanner(System.in);
+        int n = userInput.nextInt();
+
+        System.out.println("\nThe Fn of " + n + " : " + Fibonacci.fn(n));
     }
 }
